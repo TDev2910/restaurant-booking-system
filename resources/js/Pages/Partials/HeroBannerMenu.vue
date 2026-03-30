496cb2e618c2c7e5241a79a1b0277d9a20e15d01
Phạm Chí Trọng <122176490+TDev2910@users.noreply.github.com>
2026-03-30 18:02:44 +0700

    feat(menu): implement complete high-class menu page with modular CSS & responsive design (#6)

--- resources/js/Pages/Partials/HeroBannerMenu.vue
+++ resources/js/Pages/Partials/HeroBannerMenu.vue
@@ -7,59 +7,13 @@
                 Khám phá thực đơn đa dạng và hấp dẫn của chúng tôi,
                 với những món ăn ngon miệng được chế biến từ nguyên liệu tươi ngon và chất lượng.
             </p>
-            <Button label="Đặt món ngay" class="button"></Button>
         </div>
     </div>
 </template>
 
-<style scoped>
-.hero-banner {
-    height: 800px;
-    position: relative;
-}
-
-.hero-banner-bg {
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-size: cover;
-}
-
-.hero-banner-bg::before {
-    content: "";
-    position: absolute;
-    inset: 0;
-    background-color: var(--overlay-medium);
-    z-index: 1;
-    pointer-events: none;
-}
-
-.hero-banner-content {
-    z-index: 2;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    text-align: center;
-    color: var(--primary-color-600);
-    border-radius: var(--size-400);
-}
-
-.hero-banner-content p {
-    margin-inline: auto;
-    font-size: var(--fs-body);
-}
-
-.hero-banner-title {
-    font-size: var(--fs-primary-heading);
-    font-weight: var(--fw-semibold);
-    text-transform: uppercase;
-}
-</style>
+<style scoped src="@css/partials/HeroBannerMenu.css"></style>
 
 <script setup>
 import { ref } from 'vue';
-import Button from 'primevue/button';
 const bannerImg = ref('/img/hero-banner.jpeg');
 </script>
--- vite.config.js
+++ vite.config.js
@@ -21,6 +21,7 @@ export default defineConfig({
     resolve: {
         alias: {
             '@': path.resolve(__dirname, 'resources/js'),
+            '@css': path.resolve(__dirname, 'resources/css'),
         },
     },
 });
